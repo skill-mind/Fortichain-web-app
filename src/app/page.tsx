fcd1abf15d84bcd2432ed224ec829aacbff6c91c
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2025-09-03 23:00:27 +0100

    fix build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -69,9 +69,10 @@ export default function Page() {
         <div className="text-gray-text text-center mt-2">
           Subscribe to get updates on bounties, project launches, and security
           insights.
-        <div className="text-gray-text text-center mt-2">
-          Subscribe to get updates on bounties, project launches, and security
-          insights.
+          <div className="text-gray-text text-center mt-2">
+            Subscribe to get updates on bounties, project launches, and security
+            insights.
+          </div>
         </div>
       </section>
       {/* home footer */}
