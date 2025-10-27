dacaf258b8d639d973f6504198e11a87e2d2bea7
yunusabdul38 <yunusabdulmajidyunus38@gmail.com>
2025-10-27 18:25:47 +0100

    chore: chain checker

--- src/provider/walletProvider.tsx
+++ src/provider/walletProvider.tsx
@@ -37,7 +37,7 @@ export function StarknetProvider({ children }: { children: React.ReactNode }) {
       },
     }),
   ];
-  const chains = [mainnet];
+  const chains = [mainnet, sepolia];
   return (
     <StarknetConfig
       paymasterProvider={paymasterRpcProvider({
